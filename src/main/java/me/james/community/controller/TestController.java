bf523ce205e8806b00555d54efd160e96b7d580f
james <sunyjams@hotmail.com>
2021-01-14 22:18:05 +0800

    init repo

---
+++ src/main/java/me/james/community/controller/TestController.java
@@ -0,0 +1,17 @@
+package me.james.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(@RequestParam(name="name") String name, Model model){
+        model.addAttribute("name", name);
+        return "test";
+    }
+
+}
